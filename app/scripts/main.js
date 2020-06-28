40157af563bf4582f3fdfb0c9f7a9a577962f20e
Luis Zavala <cionagi@hotmail.cl>
2020-06-28 17:24:08 -0400

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,117 @@
+// Uncomment to enable Bootstrap tooltips
+// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
+// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+
+// Uncomment to enable Bootstrap popovers
+// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
+// $(function () { $('[data-toggle="popover"]').popover(); });
+
+(function($) {
+  "use strict";
+
+  $(window).on('load', function() {
+
+    /*Page Loader active
+      ========================================================*/
+    $('#preloader').fadeOut();
+
+    // Sticky Nav
+    $(window).on('scroll', function() {
+      if ($(window).scrollTop() > 200) {
+        $('.scrolling-navbar').addClass('top-nav-collapse');
+      } else {
+        $('.scrolling-navbar').removeClass('top-nav-collapse');
+      }
+    });
+
+    /* ==========================================================================
+       countdown timer
+       ========================================================================== */
+
+    /* slicknav mobile menu active  */
+    $('.mobile-menu').slicknav({
+      prependTo: '.navbar-header',
+      parentTag: 'liner',
+      allowParentLinks: true,
+      duplicate: true,
+      label: '',
+    });
+
+    /* WOW Scroll Spy
+  ========================================================*/
+    var wow = new WOW({
+      //disabled for mobile
+      mobile: false
+    });
+    wow.init();
+
+    /* Nivo Lightbox
+    ========================================================*/
+    $('.lightbox').nivoLightbox({
+      effect: 'fadeScale',
+      keyboardNav: true,
+    });
+
+    // one page navigation
+    $('.navbar-nav').onePageNav({
+      currentClass: 'active'
+    });
+
+    /* Back Top Link active
+    ========================================================*/
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 600);
+      return false;
+    });
+
+    /* Run slider simple
+     ========================================================*/
+
+    $('#slider-sponsor-container').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+  });
+
+}(jQuery));
